4dbcd61e9867399108340ea5bf927c6603ffd407
yeah <yeahleem2@gmail.com>
2021-10-01 14:12:43 +0900

    <feature> : <main 페이지 구성 중> 아직 작업중. 먼저 크롤링 작업 후, 다시 작업 예정. 211001 Noellel

--- delivery_project/front/src/components/Main.jsx
+++ delivery_project/front/src/components/Main.jsx
@@ -2,6 +2,18 @@ import React, {userEffect, userState} from 'react';
 import dish from '../images/1364668.jpg';
 
 export default function Main() {
+    let imgArray = new Array(); 
+    imgArray[0]="../images/1.png"; 
+    imgArray[1]="../images2.png";
+    imgArray[2]="../images3.png"; 
+    imgArray[3]="../images4.png"; 
+    imgArray[4]="../images5.png"; 
+    const autoImage = () => { 
+        var imgNum = Math.round(Math.random()*3); 
+        var objImg = document.getElementById("introimg"); 
+        objImg.src = imgArray[imgNum]; 
+        setTimeout(autoImage,5000); 
+    }
     return (
         <main role="main">
             <article className="intro">
@@ -15,6 +27,7 @@ export default function Main() {
             <article classname="slide">
                 <div>
                     <img src={dish} alt="접시 이미지" style={{width:1500, height:1000, objectFit:"cover"}}></img>
+                    <button onclick={autoImage}>이미지 자동변환</button>
                 </div>
             </article>
         </main>
